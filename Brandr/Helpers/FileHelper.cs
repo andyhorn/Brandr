21682136f723005c934ff62fda22571bb9ce3e18
= <=>
2019-05-30 20:22:17 -0700

    Added helper classes

---
+++ Brandr/Helpers/FileHelper.cs
@@ -0,0 +1,109 @@
+﻿using Microsoft.Win32;
+using System.IO;
+
+namespace Brandr.Helpers
+{
+    public static class FileHelper
+    {
+        public static Stream OpenFile(string filter = null)
+        {
+            var openDialog = new OpenFileDialog
+            {
+                Multiselect = false,
+                Filter = filter
+            };
+
+            var isSelected = openDialog.ShowDialog();
+
+            if (isSelected.HasValue && isSelected.Equals(true))
+            {
+                var stream = openDialog.OpenFile();
+
+                return stream;
+            }
+
+            return null;
+        }
+
+        public static Stream[] OpenFiles(string filter = null)
+        {
+            var openDialog = new OpenFileDialog
+            {
+                Multiselect = true,
+                Filter = filter
+            };
+
+            var isSelected = openDialog.ShowDialog();
+
+            if (isSelected.HasValue && isSelected.Equals(true))
+            {
+                var streams = openDialog.OpenFiles();
+
+                return streams;
+            }
+
+            return null;
+        }
+
+        public static void SaveFile(Stream stream, string filter = null, string def = null)
+        {
+            var bytes = GetBytes(stream);
+
+            SaveFile(bytes, filter, def);
+        }
+
+        public static void SaveFile(byte[] bytes, string filter = null, string def = null)
+        {
+            if (bytes == null || !(bytes.Length > 0))
+            {
+                return;
+            }
+
+            var length = bytes.Length;
+
+            var saveDialog = new SaveFileDialog();
+            saveDialog.Filter = filter;
+            saveDialog.DefaultExt = def;
+
+            saveDialog.ShowDialog();
+
+            var file = saveDialog.OpenFile();
+
+            if (file.CanWrite)
+            {
+                file.Write(bytes, 0, length);
+                file.Flush();
+                file.Close();
+            }
+        }
+
+        public static byte[] GetBytes(Stream stream)
+        {
+            if(stream == null)
+                return null;
+
+            var length = (int)stream.Length;
+
+            var buffer = new byte[length];
+
+            stream.Read(buffer, 0, length);
+
+            return buffer;
+        }
+
+        public static string GetSavePath(string filter = null, string def = ".png")
+        {
+            var saveDialog = new SaveFileDialog
+            {
+                Filter = filter,
+                DefaultExt = def
+            };
+
+            saveDialog.ShowDialog();
+
+            var path = saveDialog.FileName;
+
+            return path;
+        }
+    }
+}
---
+++ Brandr/Helpers/ImageHelper.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.ComponentModel;
+using System.IO;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace Brandr.Helpers
+{
+    public static class ImageHelper
+    {
+        public static ImageSource GetImage(byte[] data)
+        {
+            var imageConverter = new ImageSourceConverter();
+
+            var imageSource = imageConverter.ConvertFrom(data) as ImageSource;
+
+            return imageSource;
+        }
+
+        public static byte[] GetBytes(Canvas canvas, double pixelDensity = 96d)
+        {
+            double x = canvas.Margin.Left,
+                y = canvas.Margin.Top;
+
+            var background = canvas.Background as ImageBrush;
+
+            var image = background.ImageSource;
+
+            var rect = new Rect(canvas.Margin.Left, canvas.Margin.Top, image.Width, image.Height);
+
+            var bitmap = new RenderTargetBitmap((int)rect.Right, (int)rect.Bottom,
+                pixelDensity, pixelDensity, PixelFormats.Default);
+
+            bitmap.Render(canvas);
+
+            var encoder = new PngBitmapEncoder();
+
+            encoder.Frames.Add(BitmapFrame.Create(bitmap));
+
+            var stream = new MemoryStream();
+
+            encoder.Save(stream);
+            stream.Close();
+
+            var bytes = stream.ToArray();
+            return bytes;
+        }       
+    }
+}
