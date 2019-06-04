1ee48eadc91b1ee3a5f8f7ce13881e096f02a077
= <=>
2019-06-04 11:23:36 -0700

    App will now display a "preview" image that is lower quality and much smaller in file size, hopefully speeding up the edits.

--- Brandr/Helpers/Processor.cs
+++ Brandr/Helpers/Processor.cs
@@ -1,5 +1,7 @@
 ﻿using ImageProcessor;
+using ImageProcessor.Imaging.Formats;
 using System.Collections.Generic;
+using System.Drawing.Imaging;
 using System.IO;
 
 namespace Brandr.Helpers
@@ -77,6 +79,40 @@ namespace Brandr.Helpers
 
                         outputStream.Read(buffer, 0, length);
                     }
+                }
+            }
+        }
+
+        public static byte[] GetPreview(byte[] original, int quality)
+        {
+            using(var inputStream = new MemoryStream(original))
+            {
+                using(var outputStream = new MemoryStream())
+                {
+                    using(var factory = new ImageFactory())
+                    {
+                        // create a format object
+                        // the preview will use Jpeg so that we can reduce the quality
+                        // for faster rendering, the final product will use the original
+                        // format and the original quality
+                        var format = new JpegFormat
+                        {
+                            Quality = quality
+                        };
+
+                        // load the image and format it
+                        factory.Load(inputStream)
+                            .Format(format)
+                            .Save(outputStream);
+                    }
+
+                    var length = (int)outputStream.Length;
+
+                    var bytes = new byte[length];
+
+                    outputStream.Read(bytes, 0, length);
+
+                    return bytes;
                 }
             }
         }
--- Brandr/Models/BrandrImage.cs
+++ Brandr/Models/BrandrImage.cs
@@ -8,8 +8,9 @@ namespace Brandr.Models
         private readonly string ImageFilter = "Images (*.bmp;*.jpg;*.gif;*.png;*.jpeg)|*.bmp;*.jpg;*.gif;*.png;*.jpeg";
         private readonly string SaveFilter = "BMP (*.bmp)|*.bmp|JPG (*.jpg)|*.jpg|JPEG (*.jpeg)|*.jpeg|GIF (*.gif)|*.gif|PNG (*.png)|*.png|All files (*.*)|*.*";
         private readonly string DefaultSaveFormat = "png";
-        private byte[] _buffer;
-        private byte[] _display;
+        private byte[] _original;
+        private byte[] _preview;
+        private byte[] _reduced;
         private OperationList _ops;
         public List<IEditOperation> Operations { get => _ops.Operations; }
 
@@ -18,19 +19,29 @@ namespace Brandr.Models
         {
             get
             {
-                if(_buffer == null)
+                // if no image bytes are loaded, return null
+                if(_original == null)
                 {
                     return null;
                 }
-
-                if(_display == null)
+                else
                 {
-                    var length = _buffer.Length;
-                    _display = new byte[length];
-                    _buffer.CopyTo(_display, 0);
+                    
+                    //var length = _reduced.Length;
+                    //_preview = new byte[length];
+                    //_reduced.CopyTo(_preview, 0);
+                    return _preview;
                 }
 
-                return _display;
+
+                if(_reduced == null)
+                {
+                    var length = _original.Length;
+                    _reduced = new byte[length];
+                    _original.CopyTo(_reduced, 0);
+                }
+
+                return _reduced;
             }
         }
         public IEditOperation Saturation => _ops.Saturation;
@@ -45,54 +56,86 @@ namespace Brandr.Models
 
         public BrandrImage()
         {
-            _buffer = null;
-            _display = null;
+            _original = null;
+            _reduced = null;
+            _preview = null;
             _ops = new OperationList();
         }
 
         public bool LoadImage()
         {
+            // get the file bytes
             var bytes = FileHelper.GetBytes(ImageFilter);
 
-            _buffer = bytes;
+            // if nothing was loaded, return
+            if (bytes == null || bytes.Length == 0)
+            {
+                return false;
+            }
 
+            // copy the file bytes to our original cache
+            _original = bytes;
+
+            // reset any changes
             _ops.ResetAll();
 
-            return _buffer != null;
+            // get a reduced size image for the preview
+            int quality = 50;
+            _reduced = Processor.GetPreview(bytes, quality);
+            _preview = new byte[_reduced.Length];
+            _reduced.CopyTo(_preview, 0);
+
+            return _original != null &&
+                _reduced != null;
         }
 
         public void SaveImage()
         {
-            if(_display != null)
+            if(_original != null)
             {
-                FileHelper.SaveBytes(_display, SaveFilter, DefaultSaveFormat);
+                // get the list of edits
+                var edits = _ops.GetChanged();
+
+                // copy the original bytes to a new array
+                var edited = new byte[_original.Length];
+                _original.CopyTo(edited, 0);
+
+                // process all the changes
+                Processor.Process(ref edited, edits);
+
+                // save the modified bytes to a file
+                FileHelper.SaveBytes(edited, SaveFilter, DefaultSaveFormat);
             }
         }
 
         public void ProcessChanges()
         {
-            if(_buffer != null)
+            if(_reduced != null)
             {
-                byte[] bytes = null;
-
                 var edits = _ops.GetChanged();
 
+                // if there are no changes, we can return early
                 if(edits.Count == 0)
                 {
                     return;
                 }
                 else
                 {
-                    bytes = new byte[_buffer.Length];
-                    _buffer.CopyTo(bytes, 0);
+                    // get a new byte array
+                    byte[] bytes = null;
+                    bytes = new byte[_reduced.Length];
+
+                    // copy the reduced backup to the new array
+                    _reduced.CopyTo(bytes, 0);
+
+                    // process all the changes
                     Processor.Process(ref bytes, edits);
+
+                    // copy the changes into the preview for the user to see
+                    int length = bytes.Length;
+                    _preview = new byte[length];
+                    bytes.CopyTo(_preview, 0);
                 }
-
-                int length = bytes.Length;
-
-                _display = new byte[length];
-
-                bytes.CopyTo(_display, 0);
             }
         }
 
@@ -130,10 +173,11 @@ namespace Brandr.Models
 
         public void ResetAll()
         {
-            foreach(var op in Operations)
-            {
-                op.Reset();
-            }
+            _ops.ResetAll();
+            //foreach(var op in Operations)
+            //{
+            //    op.Reset();
+            //}
         }
     }
 }
